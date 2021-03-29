12dbff5185e44d2ad0693ef7c9927a3d3c3157ab
JoakimHoegaas <62018628+JoakimHoegaas@users.noreply.github.com>
2021-03-29 11:51:12 +0200

    Initial commit

---
+++ 925/Calc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _925
+{
+    public class Calc
+    {
+        private readonly double[] _workInfo;
+        private double pay;
+        private double overtimeHours;
+        private double overtimePay;
+       
+        public Calc(double[] workInfo) // [ 9, 18, 30, 1.5 ]
+        {
+            _workInfo = workInfo;
+        }
+
+        public void CalcPay()
+        {
+            pay = (17 - _workInfo[0]) * 30;
+        }
+
+        public void CalcOvertimeHours()
+        {
+            overtimeHours = _workInfo[1] - 17;
+        }
+
+        public void CalcOvertimePay()
+        {
+            overtimePay = overtimeHours * _workInfo[2] * _workInfo[3];
+        }
+
+        public double GetPayment() => pay + overtimePay;
+
+    }
+}
---
+++ 925/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _925
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc overtime = new(new double[] { 9, 18, 30, 1.5 });
+            overtime.CalcPay();
+            overtime.CalcOvertimeHours();
+            overtime.CalcOvertimePay();
+            Console.Write(overtime.GetPayment());
+        }
+    }
+}
